e0bf8cf7dd1114fc2415c790d142148675e0fa85
devianwahyu <devianwahyu666@gmail.com>
2021-06-22 11:53:16 +0700

    add text and background

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <ImageBackground 
+          source={require('./assets/ModelS.jpeg')} 
+          style={styles.image}
+        />
+        <View style={styles.titles}>
+          <Text style={styles.title}>
+            Model S
+          </Text>
+          <Text style={styles.subtitle}>
+            Starting at $69,420
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,5 +29,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
   },
 });
